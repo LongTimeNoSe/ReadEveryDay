54b816118043bf9ef351ff6e47c7c7bcedf46842
XuYanping <18310815881@163.com>
2017-03-23 17:58:35 +0800

    新增菜单栏及妹子图片保存、收藏功能

--- app/src/main/java/com/readeveryday/MyApplication.java
+++ app/src/main/java/com/readeveryday/MyApplication.java
@@ -2,6 +2,11 @@ package com.readeveryday;
 
 import android.app.Application;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readeveryday.greendao.DaoMaster;
+import com.readeveryday.greendao.DaoSession;
+import com.readeveryday.manager.GreenDaoManager;
 
 /**
  * Created by XuYanping on 2017/3/15.
@@ -15,5 +20,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+        GreenDaoManager.getGreenDaoManager(mContext);
     }
+
 }
