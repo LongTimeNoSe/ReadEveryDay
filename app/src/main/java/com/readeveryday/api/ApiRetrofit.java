1db0c536b0893a23e285ca45cbe5c9d655098af2
XuYanping <18310815881@163.com>
2017-05-15 16:28:34 +0800

    添加项目截图

--- app/src/main/java/com/readeveryday/api/ApiRetrofit.java
+++ app/src/main/java/com/readeveryday/api/ApiRetrofit.java
@@ -33,9 +33,9 @@ public class ApiRetrofit {
     public DailyApi DailyApiServer;
     public AndroidApi AndroidApiServer;
 
-    public static final String ZHIHU_BASE_URL = "http://news-at.zhihu.com/api/4/";
-    public static final String GANK_BASE_URL = "http://gank.io/api/";
-    public static final String ANDROID_BASE_URL = "http://gank.io/api/data/Android/10/";
+    public static final String ZHIHU_BASE_URL = "http://news-at.zhihu.com/Api/4/";
+    public static final String GANK_BASE_URL = "http://gank.io/Api/";
+    public static final String ANDROID_BASE_URL = "http://gank.io/Api/data/Android/10/";
     public static final String DAILY_BASE_URL = "http://app3.qdaily.com/app3/";
 
     public ZhiHuApi getZhuHuApiServer() {
